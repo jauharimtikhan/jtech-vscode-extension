6be79d81664d7bd0d9eac4486e627c87cb3d7309
jauhar-imtikhan <bangjoe.muns@gmail.com>
2026-01-09 16:44:07 +0700

    update

--- out/extension.js
+++ out/extension.js
@@ -40,19 +40,20 @@ exports.deactivate = deactivate;
 const vscode = __importStar(require("vscode"));
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
+// Tambahkan di dalam function activate()
 function activate(context) {
-    // Use the console to output diagnostic information (console.log) and errors (console.error)
-    // This line of code will only be executed once when your extension is activated
-    console.log('Congratulations, your extension "jtech-framework-support" is now active!');
-    // The command has been defined in the package.json file
-    // Now provide the implementation of the command with registerCommand
-    // The commandId parameter must match the command field in package.json
-    const disposable = vscode.commands.registerCommand('jtech-framework-support.helloWorld', () => {
-        // The code you place here will be executed every time your command is executed
-        // Display a message box to the user
-        vscode.window.showInformationMessage('Hello World from jtech-framework-support!');
-    });
-    context.subscriptions.push(disposable);
+    // ... provider lo yang lain ...
+    // Fitur: Auto Trigger Suggestion pas ngetik '<'
+    const htmlTrigger = vscode.languages.registerCompletionItemProvider("jtech", // ID bahasa lo
+    {
+        provideCompletionItems(document, position) {
+            // Return null biar VS Code nampilin snippet yang udah kita daftarin di JSON tadi
+            // Fungsinya cuma buat "membangunkan" menu suggestion
+            return undefined;
+        },
+    }, "<" // Trigger character
+    );
+    context.subscriptions.push(htmlTrigger);
 }
 // This method is called when your extension is deactivated
 function deactivate() { }
